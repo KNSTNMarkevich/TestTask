a96661a2a2c31a6def38638a510883eb4e0de7c7
KNSTNMarkevich <56442140+KNSTNMarkevich@users.noreply.github.com>
2020-01-25 03:40:18 +0300

    AddMethods_getEast/West/South/NorthMostPoint
    Add new Methods: 
    getEasternMostCity();
    getWeseternMostCity();
    getSouthernMostCity();
    getNorthenMostCity();

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
+function CityMap(str) {
 
-        function CityMaps(str) {
-            [this.city, this.abbreviation, this.latitude, this.longitude] = str.split(",")
-            
-          }
-    
-        function CityMap(str) {
-        this.list = []; 
-        str.split(";").forEach(row => {
-          this.list.push(new CityMaps(row));
-        });
-        this.NESWester = function(str){
-            switch (str) {
-                case "North":
-                    return this.list.reduce((northMostMap, currentMap) => 
-                            ({'latitude': Math.max(northMostMap.latitude, currentMap.latitude)}));           
-                case "South":
-                    return this.list.reduce(
-                        (northMostMap, currentMap) => 
-                            ({'latitude': Math.min(northMostMap.latitude, currentMap.latitude)}));
-                case "East":
-                    return this.list.reduce(
-                        (northMostMap, currentMap) => 
-                            ({'longitude': Math.max(northMostMap.longitude, currentMap.longitude)}));
-                case "West":
-                    return this.list.reduce(
-                        (northMostMap, currentMap) => 
-                            ({'longitude': Math.min(northMostMap.longitude, currentMap.longitude)}));
-                default:
-                   return alert("Enter North/West/South/West")
-            }       
-          }
-        
-          this.NewFunction = function (str){
-              return str;
-          }
-      }
-     
-    
-      var cityMap = new CityMap("Nashville, TN, 36.17, -86.78; New York, NY, 40.71, -74.00; Atlanta, GA, 33.75, -84.39; Denver, CO, 39.74, -104.98; Seattle, WA, 47.61, -122.33; Los Angeles, CA, 34.05, -118.24; Memphis, TN, 35.15, -90.05")
-      console.log(cityMap.list)
-      console.log(cityMap.NESWester("North"))
-      console.log(cityMap.NESWester("South"))
-      console.log(cityMap.NESWester("East"))
-      console.log(cityMap.NESWester("West"))
-    
+    function CityMapRecording(str) {
+        [this.city, this.latitude, this.longitude] = str.split(" ,");
+    }
+    this.list = [];
+    str.split(";").forEach(row => {
+        this.list.push(new CityMapRecording(row));
+    });   
+}
+CityMap.prototype.getEasternMostCity = function () {
+    return this.list.reduce(
+        (eastern, current) => {
+            if (!eastern || +eastern.longitude < +current.longitude)
+                return current;
+            else
+                return eastern;
+        }
+    );
+}
+CityMap.prototype.getWeseternMostCity = function () {
+    return this.list.reduce(
+        (western, current) => {
+            if (!western || -western.longitude < -current.longitude)
+                return current;
+            else
+                return western;
+        }
+    );
+}
+CityMap.prototype.getSouthernMostCity = function () {
+    return this.list.reduce(
+        (southern, current) => {
+            if (!southern || -southern.latitude < -current.latitude)
+                return current;
+            else
+                return southern;
+        }
+    );
+}
+CityMap.prototype.getNorthenMostCity = function () {
+    return this.list.reduce(
+        (northern, current) => {
+            if (!northern || +northern.latitude < +current.latitude)
+                return current;
+            else
+                return northern;
+        }
+    );
+}
+var objCityMap = new CityMap("Nashville, TN ,36.17 ,-86.78;New York, NY ,40.71 ,-74.00;Atlanta, GA ,33.75 ,-84.39;Denver, CO ,39.74 ,-104.98;Seattle, WA ,47.61 ,-122.33;Los Angeles, CA ,34.05 ,-118.24;Memphis, TN , 35.15 ,-90.05");
+console.log(objCityMap.list);
+console.log("Easternmost:", objCityMap.getEasternMostCity().city);
+console.log("Weseternmost:", objCityMap.getWeseternMostCity().city);
+console.log("Southernmost:", objCityMap.getSouthernMostCity().city);
+console.log("Northenmost:", objCityMap.getNorthenMostCity().city);
